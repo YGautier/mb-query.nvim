d3274689556028d168e3be5f659c3a7eafd36ca0
Yoan Gautier <yoan@powerz.tech>
2025-03-12 20:50:35 +0100

    Start implementing run_buf_query

--- lua/mb-query/init.lua
+++ lua/mb-query/init.lua
@@ -9,6 +9,7 @@ M.config = {
 	metabase_url = get_env("METABASE__URL"),
 	metabase_token = get_env("METABASE__TOKEN"),
 	database = 2,
+	max_nb_rows = 30,
 }
 
 function M.setup(opts)
@@ -63,6 +64,43 @@ local function make_curl_cmd(database, query, url, token)
 		payload,
 		string.format("%sapi/dataset", url),
 	}
+end
+
+---@class MetabaseQueryColumn
+--- @field display_name string
+
+--- @class MetabaseQueryResult
+--- @field rows any[][]
+--- @field cols MetabaseQueryColumn[]
+
+--- @param query_result MetabaseQueryResult
+--- @param max_nb_rows integer
+--- @return nil
+local function display_metabase_query_result(query_result, max_nb_rows)
+
+end
+
+--- @param database integer
+--- @param query string
+--- @param url string
+--- @param token string
+--- @param max_nb_rows integer
+--- @return nil
+local function query_metabase(database, query, url, token, max_nb_rows)
+	local curl_cmd = make_curl_cmd(database, query, url, token)
+
+	vim.system(curl_cmd, {}, function(out)
+		display_metabase_query_result(vim.json.decode(out.stdout).data, max_nb_rows)
+	end)
+end
+
+--- @return nil
+function M.run_buf_query()
+	local query = get_buf_sql()
+	if query == nil then
+		return nil
+	end
+	query_metabase(M.config.database, query, M.config.metabase_url, M.config.metabase_token, M.config.max_nb_rows)
 end
 
 return M
