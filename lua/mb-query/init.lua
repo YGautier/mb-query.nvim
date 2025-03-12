60fa1964c0de5a03740ca405c7535f20d508a5bb
Yoan Gautier <yoan@powerz.tech>
2025-03-12 18:54:44 +0100

    Improve module setup

--- lua/mb-query/init.lua
+++ lua/mb-query/init.lua
@@ -1,7 +1,31 @@
 local M = {}
 
+local function get_env(var)
+	return os.getenv(var)
+end
+
+M.config = {
+	metabase_url = get_env("METABASE__URL"),
+	metabase_token = get_env("METABASE__TOKEN"),
+	database = 2,
+}
+
 function M.setup(opts)
-	opts = opts or {}
+	M.config = vim.tbl_deep_extend("force", M.config, opts or {})
+
+	if not M.config.metabase_url then
+		vim.notify(
+			"Warning: Metabase URL should be declared as the 'METABASE__URL' environment variable.",
+			vim.log.levels.WARN
+		)
+	end
+
+	if not M.config.metabase_token then
+		vim.notify(
+			"Warning: Metabase TOKEN should be declared as the 'METABASE__TOKEN' environment variable.",
+			vim.log.levels.WARN
+		)
+	end
 end
 
 return M
